f2bc3c7a838d321a22faf3101e6f3a1538b52052
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2022-09-19 13:09:49 +0900

    chore: change wezterm theme

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -13,6 +13,7 @@ return {
   font = wezterm.font_with_fallback({
     'UDEV Gothic 35NFLG'
   }),
+  color_scheme = "Catppuccin Mocha",
   font_size = 10.0,
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
@@ -23,24 +24,6 @@ return {
     bottom = 0
   },
   force_reverse_video_cursor = true,
-  colors = {
-		foreground = "#dcd7ba",
-		background = "#1f1f28",
-
-		cursor_bg = "#c8c093",
-		cursor_fg = "#c8c093",
-		cursor_border = "#c8c093",
-
-		selection_fg = "#c8c093",
-		selection_bg = "#2d4f67",
-
-		scrollbar_thumb = "#16161d",
-		split = "#16161d",
-
-		ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-		brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-		indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-  },
   window_background_opacity = 0.8,
   use_fancy_tab_bar = false,
   tab_bar_style = {
